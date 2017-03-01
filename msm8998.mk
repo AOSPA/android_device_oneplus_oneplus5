6e5436eb7c2b3f7bbc4da4a5db7fe94067002580
Lida Wang <lidaw@codeaurora.org>
2017-03-01 10:01:33 -0800

    Camera: Add Vendor xml file and HIDL module
    - Add camera HIDL package into make file
    - Add camera HAL in vintf.xml
    
    Change-Id: I50b5d828f14778c6d536184b26244e19644035f4

--- msm8998.mk
+++ msm8998.mk
@@ -120,6 +120,12 @@ PRODUCT_PACKAGES += \
     android.hardware.graphics.composer@2.1-impl \
     android.hardware.graphics.composer@2.1-service
 
+# Camera configuration file. Shared by passthrough/binderized camera HAL
+PRODUCT_PACKAGES += camera.device@3.2-impl
+PRODUCT_PACKAGES += android.hardware.camera.provider@2.4-impl
+# Enable binderized camera HAL
+PRODUCT_PACKAGES += android.hardware.camera.provider@2.4-service
+
 # Sensor features
 PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.hardware.sensor.accelerometer.xml:system/etc/permissions/android.hardware.sensor.accelerometer.xml \
