be94d3118ed563c262efd72a264939cab2133945
Devi Sandeep Endluri V V <dendluri@codeaurora.org>
2017-05-03 13:05:32 -0700

    HIDL: cnd: Add cnd HALs.
    Add two HALs IApiService and IServer that are registered
    from cnd.
    
    CRs-Fixed: 2054248
    Change-Id: I0b2bf151e5899cb48277577dedd0a3085fe4be46

--- android_filesystem_config.h
+++ android_filesystem_config.h
@@ -40,6 +40,14 @@ static const struct fs_path_config android_device_files[] = {
        { 00755, AID_SYSTEM,         AID_SYSTEM,         (1ULL << CAP_NET_BIND_SERVICE), "system/bin/cnss-daemon"},
        { 00755, AID_SYSTEM,         AID_SYSTEM,         (1ULL << CAP_NET_BIND_SERVICE), "system/vendor/bin/imsdatadaemon" },
        { 00755, AID_SYSTEM,         AID_SYSTEM,         (1ULL << CAP_NET_BIND_SERVICE), "vendor/bin/imsdatadaemon" },
+       { 00755, AID_SYSTEM,         AID_SYSTEM,         CAP_MASK_LONG(CAP_NET_BIND_SERVICE) |
+                                                        CAP_MASK_LONG(CAP_BLOCK_SUSPEND) |
+                                                        CAP_MASK_LONG(CAP_NET_ADMIN),
+                                                        "vendor/bin/cnd" },
+       { 00755, AID_SYSTEM,         AID_SYSTEM,         CAP_MASK_LONG(CAP_NET_BIND_SERVICE) |
+                                                        CAP_MASK_LONG(CAP_BLOCK_SUSPEND) |
+                                                        CAP_MASK_LONG(CAP_NET_ADMIN),
+                                                        "system/vendor/bin/cnd" },
        { 00755, AID_BLUETOOTH,      AID_BLUETOOTH,      CAP_MASK_LONG(CAP_BLOCK_SUSPEND) |
                                                         CAP_MASK_LONG(CAP_NET_ADMIN),
                                                         "system/vendor/bin/hw/android.hardware.bluetooth@1.0-service-qti" },
