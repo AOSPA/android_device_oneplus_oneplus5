2a8f8109d35636d8a468d5e124f776d960bc985f
Alex Naidis <alex.naidis@linux.com>
2017-07-07 22:51:00 +0200

    oneplus5: Switch to selinux permissive
    Signed-off-by: Alex Naidis <alex.naidis@linux.com>

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -52,6 +52,9 @@ TARGET_USES_COLOR_METADATA := true
 BOARD_KERNEL_CMDLINE += console=ttyMSM0,115200,n8 androidboot.console=ttyMSM0 earlycon=msm_serial_dm,0xc1b0000
 BOARD_KERNEL_CMDLINE += androidboot.hardware=qcom user_debug=31 msm_rtb.filter=0x237 ehci-hcd.park=3 lpm_levels.sleep_disabled=1 sched_enable_hmp=1 sched_enable_power_aware=1 service_locator.enable=1 swiotlb=2048 androidboot.configfs=true androidboot.usbcontroller=a800000.dwc3
 
+# STOPSHIP: permissive
+BOARD_KERNEL_CMDLINE += androidboot.selinux=permissive
+
 BOARD_KERNEL_BASE        := 0x00000000
 BOARD_KERNEL_PAGESIZE    := 4096
 BOARD_KERNEL_TAGS_OFFSET := 0x01E00000
