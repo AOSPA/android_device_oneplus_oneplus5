ae8df563bc01d4b1e25303fea369c4334e88559d
Willi Ye <williye97@gmail.com>
2017-07-07 13:41:10 +0200

    oneplus5: Clean up makefile
    Signed-off-by: Alex Naidis <alex.naidis@linux.com>

--- oneplus5.mk
+++ oneplus5.mk
@@ -1,25 +1,20 @@
 DEVICE_PACKAGE_OVERLAYS := device/oneplus/oneplus5/overlay
-TARGET_KERNEL_VERSION := 4.4
-BOARD_HAVE_QCOM_FM := true
 TARGET_USES_NQ_NFC := true
 
-ifeq ($(TARGET_USES_NQ_NFC),true)
 # Flag to enable and support NQ3XX chipsets
 NQ3XX_PRESENT := true
-endif
 
 TARGET_ENABLE_QC_AV_ENHANCEMENTS := true
-BOARD_FRP_PARTITION_NAME :=frp
+BOARD_FRP_PARTITION_NAME := frp
 
 # enable the SVA in UI area
 TARGET_USE_UI_SVA := true
 
 # Video codec configuration files
-ifeq ($(TARGET_ENABLE_QC_AV_ENHANCEMENTS), true)
-PRODUCT_COPY_FILES += device/oneplus/oneplus5/media/media_profiles.xml:system/etc/media_profiles.xml \
-                      device/oneplus/oneplus5/media/media_codecs.xml:system/etc/media_codecs.xml \
-                      device/oneplus/oneplus5/media/media_codecs_performance.xml:system/etc/media_codecs_performance.xml
-endif #TARGET_ENABLE_QC_AV_ENHANCEMENTS
+PRODUCT_COPY_FILES += \
+    device/oneplus/oneplus5/media/media_profiles.xml:system/etc/media_profiles.xml \
+    device/oneplus/oneplus5/media/media_codecs.xml:system/etc/media_codecs.xml \
+    device/oneplus/oneplus5/media/media_codecs_performance.xml:system/etc/media_codecs_performance.xml
 
 #QTIC flag
 -include $(QCPATH)/common/config/qtic-config.mk
@@ -50,25 +45,21 @@ PRODUCT_BOOT_JARS += WfdCommon
 PRODUCT_BOOT_JARS += oem-services
 endif
 
-ifeq ($(strip $(BOARD_HAVE_QCOM_FM)),true)
-PRODUCT_BOOT_JARS += qcom.fmradio
-endif #BOARD_HAVE_QCOM_FM
-
 # Audio configuration file
 -include $(TOPDIR)hardware/qcom/audio/configs/msm8998/msm8998.mk
 
-PRODUCT_PACKAGE_OVERLAYS := $(QCPATH)/qrdplus/Extension/res \
-        $(QCPATH)/qrdplus/globalization/multi-language/res-overlay \
-        $(PRODUCT_PACKAGE_OVERLAYS)
+PRODUCT_COPY_FILES += \
+    device/oneplus/oneplus5/audio/mixer_paths_tasha.xml:system/etc/mixer_paths_tasha.xml \
+    device/oneplus/oneplus5/audio/audio_platform_info.xml:system/etc/audio_platform_info.xml
+
+PRODUCT_PACKAGE_OVERLAYS := \
+    $(QCPATH)/qrdplus/Extension/res \
+    $(QCPATH)/qrdplus/globalization/multi-language/res-overlay \
+    $(PRODUCT_PACKAGE_OVERLAYS)
 
 # Sensor HAL conf file
 PRODUCT_COPY_FILES += \
     device/oneplus/oneplus5/sensors/hals.conf:system/etc/sensors/hals.conf
-
-# WLAN host driver
-ifneq ($(WLAN_CHIPSET),)
-PRODUCT_PACKAGES += $(WLAN_CHIPSET)_wlan.ko
-endif
 
 # WLAN driver configuration file
 PRODUCT_COPY_FILES += \
@@ -76,7 +67,8 @@ PRODUCT_COPY_FILES += \
     device/oneplus/oneplus5/wifi/wifi_concurrency_cfg.txt:system/etc/wifi/wifi_concurrency_cfg.txt
 
 # MIDI feature
-PRODUCT_COPY_FILES += frameworks/native/data/etc/android.software.midi.xml:system/etc/permissions/android.software.midi.xml
+PRODUCT_COPY_FILES += \
+    frameworks/native/data/etc/android.software.midi.xml:system/etc/permissions/android.software.midi.xml
 
 PRODUCT_PACKAGES += \
     wpa_supplicant_overlay.conf \
@@ -88,7 +80,6 @@ PRODUCT_PACKAGES += \
     libantradio \
     antradio_app \
     libvolumelistener
-
 
 # Sensor features
 PRODUCT_COPY_FILES += \
@@ -109,36 +100,20 @@ PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.hardware.vr.high_performance.xml:system/etc/permissions/android.hardware.vr.high_performance.xml
 
 # MSM IRQ Balancer configuration file
-PRODUCT_COPY_FILES += device/oneplus/oneplus5/msm_irqbalance.conf:system/vendor/etc/msm_irqbalance.conf
+PRODUCT_COPY_FILES += \
+    device/oneplus/oneplus5/msm_irqbalance.conf:system/vendor/etc/msm_irqbalance.conf
 
 # Powerhint configuration file
 PRODUCT_COPY_FILES += \
-device/oneplus/oneplus5/powerhint_soc_id_292.xml:system/etc/powerhint_soc_id_292.xml
+    device/oneplus/oneplus5/powerhint_soc_id_292.xml:system/etc/powerhint_soc_id_292.xml
 
 #for android_filesystem_config.h
 PRODUCT_PACKAGES += \
     fs_config_files
 
-# dm-verity configuration
-PRODUCT_SUPPORTS_VERITY := true
-PRODUCT_SYSTEM_VERITY_PARTITION := /dev/block/bootdevice/by-name/system
-
 # List of AAPT configurations
 PRODUCT_AAPT_CONFIG += xlarge large
-#A/B related packages
-PRODUCT_PACKAGES += update_engine \
-		    update_engine_client \
-		    update_verifier \
-		    bootctrl.msm8998 \
-		    brillo_update_payload
-#Boot control HAL test app
-PRODUCT_PACKAGES_DEBUG += bootctl
 
 #FEATURE_OPENGLES_EXTENSION_PACK support string config file
 PRODUCT_COPY_FILES += \
-	frameworks/native/data/etc/android.hardware.opengles.aep.xml:system/etc/permissions/android.hardware.opengles.aep.xml
-
-# Audio Configuration
-PRODUCT_COPY_FILES += \
-    device/oneplus/oneplus5/audio/mixer_paths_tasha.xml:system/etc/mixer_paths_tasha.xml \
-    device/oneplus/oneplus5/audio/audio_platform_info.xml:system/etc/audio_platform_info.xml
+    frameworks/native/data/etc/android.hardware.opengles.aep.xml:system/etc/permissions/android.hardware.opengles.aep.xml
