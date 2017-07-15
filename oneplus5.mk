246155385b746a1c5c983c5ff07cf32445c9bf22
thecrazyskull <anaskarbila@gmail.com>
2017-07-15 16:01:09 -0400

    oneplus5: build fingerprint & move gps configs to vendor

--- oneplus5.mk
+++ oneplus5.mk
@@ -85,11 +85,11 @@ PRODUCT_PACKAGES += \
     gps.msm8998
 
 PRODUCT_COPY_FILES += \
-    $(LOCAL_PATH)/gps/etc/flp.conf:system/etc/flp.conf \
-    $(LOCAL_PATH)/gps/etc/izat.conf:system/etc/izat.conf \
-    $(LOCAL_PATH)/gps/etc/lowi.conf:system/etc/lowi.conf \
-    $(LOCAL_PATH)/gps/etc/sap.conf:system/etc/sap.conf \
-    $(LOCAL_PATH)/gps/etc/xtwifi.conf:system/etc/xtwifi.conf
+    $(LOCAL_PATH)/gps/etc/flp.conf:system/vendor/etc/flp.conf \
+    $(LOCAL_PATH)/gps/etc/izat.conf:system/vendor/etc/izat.conf \
+    $(LOCAL_PATH)/gps/etc/lowi.conf:system/vendor/etc/lowi.conf \
+    $(LOCAL_PATH)/gps/etc/sap.conf:system/vendor/etc/sap.conf \
+    $(LOCAL_PATH)/gps/etc/xtwifi.conf:system/vendor/etc/xtwifi.conf
 
 # Sensor features
 PRODUCT_COPY_FILES += \
@@ -123,6 +123,12 @@ PRODUCT_PACKAGES += \
 
 # List of AAPT configurations
 PRODUCT_AAPT_CONFIG += xlarge large
+
+#Android fingerprint daemon implementation
+PRODUCT_PACKAGES += fingerprintd
+
+PRODUCT_COPY_FILES += \
+    frameworks/native/data/etc/android.hardware.fingerprint.xml:system/etc/permissions/android.hardware.fingerprint.xml
 
 #FEATURE_OPENGLES_EXTENSION_PACK support string config file
 PRODUCT_COPY_FILES += \
