7414d9c0b10be0d8304d9f00407564c947cbe423
Hernán Castañón <herna@paranoidandroid.co>
2017-11-29 23:17:29 +0100

    oneplus5: disable dexpreopt(temporal)
    Signed-off-by: Hernán Castañón <herna@paranoidandroid.co>

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -18,6 +18,9 @@ TARGET_2ND_CPU_ABI := armeabi-v7a
 TARGET_2ND_CPU_ABI2 := armeabi
 TARGET_2ND_CPU_VARIANT := cortex-a9
 
+WITH_DEXPREOPT := false
+WITH_DEXPREOPT_PIC := false
+
 #Enable HW based full disk encryption
 TARGET_HW_DISK_ENCRYPTION := true
 TARGET_CRYPTFS_HW_PATH := device/qcom/common/cryptfs_hw
