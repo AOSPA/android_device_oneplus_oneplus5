e90c37ad5cdf4e6020928de0fea02791fbaf02ff
Hernán Castañón <herna@paranoidandroid.co>
2018-01-14 21:49:26 +0100

    init: enable software keys for the OnePlus5T
    Not sure if there is a better way for doing this, but this
    should work as of now.
    
    Signed-off-by: Hernán Castañón <herna@paranoidandroid.co>

--- init/init_oneplus5.cpp
+++ init/init_oneplus5.cpp
@@ -43,6 +43,7 @@ void init_msm_properties(unsigned long msm_id, unsigned long msm_ver, char *boar
         property_set("ro.power_profile", "/system/vendor/etc/power_profile_5t.xml");
         property_set("ro.product.device", "OnePlus5T");
         property_set("ro.product.model", "ONEPLUS A5010");
+        property_set("qemu.hw.mainkeys", "0");
    } else {
         property_set("ro.power_profile", "/system/vendor/etc/power_profile.xml");
         property_set("ro.product.device", "OnePlus5");
