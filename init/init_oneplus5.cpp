0af3bb78df4040c6a5980d0039420c0510dcb50a
Willi Ye <williye97@gmail.com>
2018-01-18 22:07:02 +0100

    init: Set model name

--- init/init_oneplus5.cpp
+++ init/init_oneplus5.cpp
@@ -42,8 +42,10 @@ void init_msm_properties(unsigned long msm_id, unsigned long msm_ver, char *boar
     if (project_name == "17801") {
         property_set("ro.power_profile", "/system/vendor/etc/power_profile_5t.xml");
         property_set("ro.product.device", "OnePlus5T");
+        property_set("ro.product.model", "ONEPLUS A5010");
    } else {
         property_set("ro.power_profile", "/system/vendor/etc/power_profile.xml");
         property_set("ro.product.device", "OnePlus5");
+        property_set("ro.product.model", "ONEPLUS A5000");
    }
 }
