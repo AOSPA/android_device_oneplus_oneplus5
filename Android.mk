2526d62f8a8f1aad734c6bc014395810299516a3
Willi Ye <williye97@gmail.com>
2018-01-18 21:57:35 +0100

    Create symlinks

--- Android.mk
+++ Android.mk
@@ -1,3 +1,63 @@
 ifeq ($(TARGET_DEVICE),oneplus5)
-include $(all-subdir-makefiles)
+
+#Create symbolic links
+$(shell mkdir -p $(TARGET_OUT_VENDOR_ETC)/firmware/wcd9320; \
+	ln -sf /data/misc/audio/wcd9320_anc.bin \
+		$(TARGET_OUT_VENDOR_ETC)/firmware/wcd9320/wcd9320_anc.bin; \
+	ln -sf /data/misc/audio/wcd9320_mad_audio.bin \
+		$(TARGET_OUT_VENDOR_ETC)/firmware/wcd9320/wcd9320_mad_audio.bin; \
+	ln -sf /data/misc/audio/mbhc.bin \
+		$(TARGET_OUT_VENDOR_ETC)/firmware/wcd9320/wcd9320_mbhc.bin; \
+	ln -sf /dev/block/bootdevice/by-name/msadp \
+		$(TARGET_OUT_VENDOR)/firmware/msadp)
+
+RFS_MSM_ADSP_SYMLINKS := $(TARGET_OUT_VENDOR)/rfs/msm/adsp/
+$(RFS_MSM_ADSP_SYMLINKS): $(LOCAL_INSTALLED_MODULE)
+	@echo "Creating RFS MSM ADSP folder structure: $@"
+	@rm -rf $@/*
+	@mkdir -p $(dir $@)/readonly/vendor
+	$(hide) ln -sf /data/vendor/tombstones/rfs/lpass $@/ramdumps
+	$(hide) ln -sf /persist/rfs/msm/adsp $@/readwrite
+	$(hide) ln -sf /persist/rfs/shared $@/shared
+	$(hide) ln -sf /persist/hlos_rfs/shared $@/hlos
+	$(hide) ln -sf /firmware $@/readonly/firmware
+	$(hide) ln -sf /vendor/firmware $@/readonly/vendor/firmware
+
+RFS_MSM_MPSS_SYMLINKS := $(TARGET_OUT_VENDOR)/rfs/msm/mpss/
+$(RFS_MSM_MPSS_SYMLINKS): $(LOCAL_INSTALLED_MODULE)
+	@echo "Creating RFS MSM MPSS folder structure: $@"
+	@rm -rf $@/*
+	@mkdir -p $(dir $@)/readonly/vendor
+	$(hide) ln -sf /data/vendor/tombstones/rfs/modem $@/ramdumps
+	$(hide) ln -sf /persist/rfs/msm/mpss $@/readwrite
+	$(hide) ln -sf /persist/rfs/shared $@/shared
+	$(hide) ln -sf /persist/hlos_rfs/shared $@/hlos
+	$(hide) ln -sf /firmware $@/readonly/firmware
+	$(hide) ln -sf /vendor/firmware $@/readonly/vendor/firmware
+
+RFS_MSM_SLPI_SYMLINKS := $(TARGET_OUT_VENDOR)/rfs/msm/slpi/
+$(RFS_MSM_SLPI_SYMLINKS): $(LOCAL_INSTALLED_MODULE)
+	@echo "Creating RFS MSM SLPI folder structure: $@"
+	@rm -rf $@/*
+	@mkdir -p $(dir $@)/readonly
+	$(hide) ln -sf /data/vendor/tombstones/rfs/slpi $@/ramdumps
+	$(hide) ln -sf /persist/rfs/msm/slpi $@/readwrite
+	$(hide) ln -sf /persist/rfs/shared $@/shared
+	$(hide) ln -sf /persist/hlos_rfs/shared $@/hlos
+	$(hide) ln -sf /firmware $@/readonly/firmware
+
+ALL_DEFAULT_INSTALLED_MODULES += $(RFS_MSM_ADSP_SYMLINKS) $(RFS_MSM_MPSS_SYMLINKS) $(RFS_MSM_SLPI_SYMLINKS)
+
+# Read WiFi MAC Address from persist partition
+$(shell mkdir -p $(TARGET_OUT_VENDOR)/firmware/wlan/qca_cld ; \
+	ln -sf /persist/wlan_mac.bin \
+		$(TARGET_OUT_VENDOR)/firmware/wlan/qca_cld/wlan_mac.bin; \
+	ln -sf /vendor/etc/wifi/WCNSS_qcom_cfg.ini \
+		$(TARGET_OUT_VENDOR)/firmware/wlan/qca_cld/WCNSS_qcom_cfg.ini)
+
+#Create dsp directory
+$(shell mkdir -p $(TARGET_OUT_VENDOR)/lib/dsp)
+
+include $(call all-subdir-makefiles)
+
 endif
--- AndroidBoard.mk
+++ AndroidBoard.mk
@@ -1,18 +1,1 @@
 LOCAL_PATH := $(call my-dir)
-
-#----------------------------------------------------------------------
-# Copy additional target-specific files
-#----------------------------------------------------------------------
-
-# Create symbolic links for WLAN
-$(shell mkdir -p $(TARGET_OUT_VENDOR)/firmware/wlan/qca_cld; \
-ln -sf /vendor/etc/wifi/WCNSS_qcom_cfg.ini \
-$(TARGET_OUT_VENDOR)/firmware/wlan/qca_cld/WCNSS_qcom_cfg.ini)
-
-#Create dsp directory
-$(shell mkdir -p $(TARGET_OUT_VENDOR)/lib/dsp)
-
-# Create symbolic links for msadp
-$(shell  mkdir -p $(TARGET_OUT_VENDOR)/firmware; \
-	ln -sf /dev/block/bootdevice/by-name/msadp \
-	$(TARGET_OUT_VENDOR)/firmware/msadp)
