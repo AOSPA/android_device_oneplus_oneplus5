f12342d10a79d6b167a80f9f15218edadcd69aff
Marco Marinho <marcomarinho12@gmail.com>
2020-07-14 12:09:14 +0100

    oneplus5(t): Remove framework manifest
    * unlikely this leads to bootloops which the cause will be investigated after code freeze. for now, let's just remove that
    
    Signed-off-by: Marco Marinho <marcomarinho12@gmail.com>
    Change-Id: I9899ad757075f7c8650f2c8e4a1a4cfe9dbcdb16

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -99,9 +99,6 @@ TARGET_ENABLE_MEDIADRM_64 := true
 # Filesystem
 TARGET_FS_CONFIG_GEN := $(COMMON_PATH)/config.fs
 
-# HIDL
-DEVICE_FRAMEWORK_MANIFEST_FILE := $(COMMON_PATH)/framework_manifest.xml
-
 # Gestures
 TARGET_GESTURES_NODE := "/proc/touchpanel/gesture_enable"
 
