49a34edc704abed9e83fe05ecc0b3efd52d7e9b7
Marco Marinho <marcomarinho12@gmail.com>
2020-08-07 19:50:26 +0100

    oneplus5(t): init: Remove init.target.rc
    * this wasn't done in the right way and it's useless since we can do it in init.pa.rc
    
    Change-Id: I9a5730252df30eeb626f45aa80affb582fcb06d0

--- rootdir/Android.mk
+++ rootdir/Android.mk
@@ -25,14 +25,6 @@ LOCAL_MODULE_PATH := $(TARGET_OUT_VENDOR_OVERLAY_EXECUTABLES)
 include $(BUILD_PREBUILT)
 
 include $(CLEAR_VARS)
-LOCAL_MODULE       := init.target.rc
-LOCAL_MODULE_TAGS  := optional
-LOCAL_MODULE_CLASS := ETC
-LOCAL_SRC_FILES    := etc/init.target.rc
-LOCAL_MODULE_PATH  := $(TARGET_OUT_ETC)/init
-include $(BUILD_PREBUILT)
-
-include $(CLEAR_VARS)
 LOCAL_MODULE       := ueventd.qcom.rc
 LOCAL_MODULE_TAGS  := optional
 LOCAL_MODULE_CLASS := ETC
