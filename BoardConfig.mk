833b31a008a3debc236af4ad184091f8807761b5
Marco Marinho <marcomarinho12@gmail.com>
2020-09-19 20:49:57 +0100

    oneplus5(t): Restore back some configs
    * our cpu variant it's not kryo but instead the most adequated is cortex-a73
    * our device it's not full treble so this brought up some unstability and restrictions with sepolicy since we couldn't do it in the right way
    ** this is based on a whole A10 experience and tests that were done over the time, so let's stabilize for a final release :)
    
    Signed-off-by: Marco Marinho <marcomarinho12@gmail.com>
    Change-Id: I956d599ecb0f9c00055071152631d87b90cfe279

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -27,13 +27,13 @@ TARGET_ARCH := arm64
 TARGET_ARCH_VARIANT := armv8-a
 TARGET_CPU_ABI := arm64-v8a
 TARGET_CPU_ABI2 :=
-TARGET_CPU_VARIANT := kryo
+TARGET_CPU_VARIANT := cortex-a73
 
 TARGET_2ND_ARCH := arm
 TARGET_2ND_ARCH_VARIANT := armv8-a
 TARGET_2ND_CPU_ABI := armeabi-v7a
 TARGET_2ND_CPU_ABI2 := armeabi
-TARGET_2ND_CPU_VARIANT := kryo
+TARGET_2ND_CPU_VARIANT := cortex-a73
 
 # Bootloader
 TARGET_BOOTLOADER_BOARD_NAME := msm8998
@@ -126,13 +126,16 @@ TARGET_RECOVERY_FSTAB := $(COMMON_PATH)/rootdir/etc/fstab.qcom
 TARGET_OVERLAYS_POWERHAL := true
 TARGET_USES_INTERACTION_BOOST := false
 
+# Properties
+BOARD_PROPERTY_OVERRIDES_SPLIT_ENABLED := true
+
 # Sepolicy
 BOARD_PLAT_PRIVATE_SEPOLICY_DIR += $(COMMON_PATH)/sepolicy/private
 
 # Treble
 BOARD_VNDK_VERSION := current
-PRODUCT_FULL_TREBLE_OVERRIDE := true
 PRODUCT_TARGET_VNDK_VERSION := 29
+PRODUCT_SEPOLICY_SPLIT := true
 
 # inherit from the proprietary version
 -include vendor/oneplus/oneplus5/BoardConfigVendor.mk
