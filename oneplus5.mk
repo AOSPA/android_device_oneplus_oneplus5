0b6617124b8ba0fbe3b529abc9638620fe736f75
Shreyas Narayan <shrena@codeaurora.org>
2020-12-14 15:48:53 +0530

    oneplus5: Remove legacy DRM HAL
    Change-Id: I7fc75685808ae83d4a1552694545cc7a020b6639

--- oneplus5.mk
+++ oneplus5.mk
@@ -118,8 +118,6 @@ PRODUCT_PROPERTY_OVERRIDES += \
 
 # DRM
 PRODUCT_PACKAGES += \
-    android.hardware.drm@1.0-impl:64 \
-    android.hardware.drm@1.0-service \
     android.hardware.drm@1.4.vendor \
     android.hardware.drm@1.4-service.clearkey
 
