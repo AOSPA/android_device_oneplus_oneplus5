0529de402ef12969c0ba1894044970e26a9fd284
Adithya R <gh0strider.2k18.reborn@gmail.com>
2021-04-30 14:28:12 +0530

    oneplus5: Add atcmdfwd HAL to framework manifest
    Fixes:
    
    04-30 14:19:49.509   569   569 I hwservicemanager: getTransport: Cannot find entry vendor.qti.hardware.radio.atcmdfwd@1.0::IAtCmdFwd/AtCmdFwdService in either framework or device manifest.
    04-30 14:19:49.509  5546  5546 E HidlServiceManagement: Service vendor.qti.hardware.radio.atcmdfwd@1.0::IAtCmdFwd/AtCmdFwdService must be in VINTF manifest in order to register/get.
    04-30 14:19:49.509  5546  5546 E AtFwdService: Starting AtCmdFwd Service
    04-30 14:19:49.509  5546  5546 E AtFwdService: android.os.RemoteException: HwBinder Error: (-2147483648)
    04-30 14:19:49.509  5546  5546 E AtFwdService: 	at android.os.HwBinder.registerService(Native Method)
    04-30 14:19:49.509  5546  5546 E AtFwdService: 	at vendor.qti.hardware.radio.atcmdfwd.V1_0.IAtCmdFwd$Stub.registerAsService(IAtCmdFwd.java:413)
    04-30 14:19:49.509  5546  5546 E AtFwdService: 	at com.qualcomm.atfwd.AtFwdService.onCreate(AtFwdService.java:29)
    04-30 14:19:49.509  5546  5546 E AtFwdService: 	at android.app.ActivityThread.handleCreateService(ActivityThread.java:4186)
    04-30 14:19:49.509  5546  5546 E AtFwdService: 	at android.app.ActivityThread.access$1500(ActivityThread.java:237)
    04-30 14:19:49.509  5546  5546 E AtFwdService: 	at android.app.ActivityThread$H.handleMessage(ActivityThread.java:1932)
    04-30 14:19:49.509  5546  5546 E AtFwdService: 	at android.os.Handler.dispatchMessage(Handler.java:106)
    04-30 14:19:49.509  5546  5546 E AtFwdService: 	at android.os.Looper.loop(Looper.java:223)
    04-30 14:19:49.509  5546  5546 E AtFwdService: 	at android.app.ActivityThread.main(ActivityThread.java:7661)
    04-30 14:19:49.509  5546  5546 E AtFwdService: 	at java.lang.reflect.Method.invoke(Native Method)
    04-30 14:19:49.509  5546  5546 E AtFwdService: 	at com.android.internal.os.RuntimeInit$MethodAndArgsCaller.run(RuntimeInit.java:594)
    04-30 14:19:49.509  5546  5546 E AtFwdService: 	at com.android.internal.os.ZygoteInit.main(ZygoteInit.java:947)

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -48,6 +48,7 @@ DEVICE_MANIFEST_FILE += \
     $(PLATFORM_PATH)/manifest.xml \
     $(PLATFORM_PATH)/oneplus_manifest.xml
 DEVICE_MATRIX_FILE += device/qcom/common/compatibility_matrix.xml
+DEVICE_FRAMEWORK_MANIFEST_FILE += $(PLATFORM_PATH)/framework_manifest.xml
 DEVICE_FRAMEWORK_COMPATIBILITY_MATRIX_FILE += \
     $(PLATFORM_PATH)/oneplus_vendor_framework_compatibility_matrix.xml \
     vendor/qcom/opensource/core-utils/vendor_framework_compatibility_matrix.xml
