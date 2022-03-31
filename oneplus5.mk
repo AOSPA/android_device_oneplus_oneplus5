3b2343a3a44af751cddef60a12b55fd47b1e5118
Kunmun <kunmun.devroms@gmail.com>
2022-03-31 23:26:06 +0530

    oneplus5: Import common configs from LA.UM.10.2.1.r1-03400-sdm660.0
    Change-Id: I540e6cccbaf1dd9c83c9f8b2c71d31d700824d81

--- oneplus5.mk
+++ oneplus5.mk
@@ -4,11 +4,23 @@
 # SPDX-License-Identifier: Apache-2.0
 #
 
+# AID/fs configs
+PRODUCT_PACKAGES += \
+    fs_config_files
+
 # Board Platform
 TARGET_BOARD_PLATFORM := msm8998
 
+# IPC router config
+PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/configs/sec_config:$(TARGET_COPY_OUT_VENDOR)/etc/sec_config
+
 # Kernel
 TARGET_KERNEL_VERSION := 4.4
+
+# Public Libraries
+PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/configs/public.libraries.txt:$(TARGET_COPY_OUT_VENDOR)/etc/public.libraries.txt
 
 # Soong namespaces
 PRODUCT_SOONG_NAMESPACES += $(LOCAL_PATH)
