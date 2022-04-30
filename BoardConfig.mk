338e178f7795208483bcaed2c1efe5ad22b24163
Divyanshu-Modi <divyan.m05@gmail.com>
2022-04-30 10:22:14 +0400

    oneplus5: BoardConfig: Disable kpti
    Upstream kernels have a kpti safe list and cortex a73 and cortex-a53 are in the list.
    Hence, are safe from meltdown and spectre attack So... disable kpti in favour of increased perf.
    
    link to kpti safe list:
    https://github.com/Divyanshu-Modi/linux-5.x/blob/v5.18/arch/arm64/kernel/cpufeature.c#L1525
    
    Signed-off-by: Divyanshu-Modi <divyan.m05@gmail.com>

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -58,7 +58,7 @@ TARGET_INIT_VENDOR_LIB := //$(PLATFORM_PATH):libinit_oneplus5
 # Kernel
 BOARD_KERNEL_CMDLINE := androidboot.hardware=qcom user_debug=31 msm_rtb.filter=0x37 ehci-hcd.park=3
 BOARD_KERNEL_CMDLINE += androidboot.configfs=true androidboot.usbcontroller=a800000.dwc3
-BOARD_KERNEL_CMDLINE += service_locator.enable=1 swiotlb=2048 loop.max_part=7
+BOARD_KERNEL_CMDLINE += service_locator.enable=1 swiotlb=2048 loop.max_part=7 kpti=off
 #BOARD_KERNEL_CMDLINE += androidboot.selinux=permissive
 BOARD_KERNEL_BASE := 0x00000000
 BOARD_KERNEL_PAGESIZE := 4096
