3b0933abbc7f91929d5efc4926c61eaed52b8fd8
enesykaya <enesykaya@gmail.com>
2022-09-24 23:28:45 +0300

    Revert "oneplus5: Add atcmdfwd HAL to framework manifest"
    This reverts commit 0529de402ef12969c0ba1894044970e26a9fd284.

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -44,7 +44,6 @@ DEVICE_MANIFEST_FILE += \
     $(PLATFORM_PATH)/configs/vintf/manifest.xml \
     $(PLATFORM_PATH)/configs/vintf/oneplus_manifest.xml
 DEVICE_MATRIX_FILE += device/qcom/common/compatibility_matrix.xml
-DEVICE_FRAMEWORK_MANIFEST_FILE += $(PLATFORM_PATH)/configs/vintf/framework_manifest.xml
 DEVICE_FRAMEWORK_COMPATIBILITY_MATRIX_FILE += \
     $(PLATFORM_PATH)/configs/vintf/oneplus_vendor_framework_compatibility_matrix.xml \
     vendor/qcom/opensource/core-utils/vendor_framework_compatibility_matrix.xml
