ce949b1a039beea1d349604936444d6344994195
enesykaya <enesykaya@gmail.com>
2023-01-05 18:16:50 +0300

    oneplus5: remove_packages: Drop soundpicker
    * oops

--- remove_packages/Android.mk
+++ remove_packages/Android.mk
@@ -4,7 +4,7 @@ include $(CLEAR_VARS)
 LOCAL_MODULE := RemovePackages
 LOCAL_MODULE_CLASS := APPS
 LOCAL_MODULE_TAGS := optional
-LOCAL_OVERRIDES_PACKAGES := FilesPrebuilt Maps PixelDocumentsUIGoogleOverlay PixelDocumentsUIOverlay SoundPickerPrebuilt YouTube
+LOCAL_OVERRIDES_PACKAGES := FilesPrebuilt Maps PixelDocumentsUIGoogleOverlay PixelDocumentsUIOverlay YouTube
 LOCAL_UNINSTALLABLE_MODULE := true
 LOCAL_CERTIFICATE := PRESIGNED
 LOCAL_SRC_FILES := /dev/null
