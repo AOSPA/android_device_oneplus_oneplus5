855258844541f36e0392ccbe2ec0a76740cb3b8e
enesykaya <enesykaya@gmail.com>
2023-01-05 19:10:02 +0300

    oneplus5: Switch to aosp keymaster/gatekeeper

--- oneplus5.mk
+++ oneplus5.mk
@@ -150,8 +150,13 @@ PRODUCT_COPY_FILES += \
 
 # Keymaster
 PRODUCT_PACKAGES += \
-    android.hardware.keymaster@3.0.vendor \
-    android.hardware.gatekeeper@1.0.vendor
+    android.hardware.gatekeeper@1.0-impl \
+    android.hardware.gatekeeper@1.0-service \
+    android.hardware.gatekeeper@1.0.vendor \
+    android.hardware.keymaster@3.0-impl \
+    android.hardware.keymaster@3.0-service \
+    android.hardware.keymaster@3.0.vendor
+
 
 # Lights
 PRODUCT_PACKAGES += \
